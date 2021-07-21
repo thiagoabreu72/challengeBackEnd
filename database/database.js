a465848e7ff06b473928473904986276c520cb4c
Thiago Abreu <thiagoabreu72@gmail.com>
2021-07-21 12:36:11 -0300

    Implementando os primeiros códigos e organização

---
+++ database/database.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+class Database {
+  constructor() {
+    this.conectDb();
+  }
+
+  conectDb() {
+    (async () => {
+      try {
+        await mongoose.connect(process.env.DB_MONGO, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        });
+        console.log("Conectado ao MongoDB. \n");
+      } catch (error) {
+        console.log("Erro ao conectar com o MONGODB " + error);
+      }
+    })();
+  }
+}
+
+export default new Database();
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import router from "./routes/router.js";
+import router from "./api/routes/router.js";
 
 const app = express();
 app.use(express.json());
